7c078529e4643c997dac73c2fa27f027ddb9e8e8
Amos Friling <friling@gmail.com>
2024-08-30 16:28:32 -0300

    With buttons

--- app.py
+++ app.py
@@ -2,15 +2,34 @@ from re import DEBUG
 
 from flask import Flask, render_template
 
+#https://www.youtube.com/watch?v=yBDHkveJUf4
+
+
 app = Flask(__name__)
 
+FILE_SOURCE = [
+    {'id': '1',
+     'company': 'IDEAL',
+     'start_line': '5',
+     'product_column': '3',
+     'base_price_column': '9',
+     'list_price_column': '12',
+     'replacement_cost_column': '24',
+     'last_forign_cost_column': '24'},
+     {'id': '2',
+     'company': 'Hoffman Canada',
+     'start_line': '5',
+     'product_column': '3',
+     'base_price_column': '9',
+     'list_price_column': '12',
+     'replacement_cost_column': '24',
+     'last_forign_cost_column': '24'}
+    ]
 
 @app.route('/')
 def hello_world():
-    #return "AAAA"
 
-
-    return render_template('home.html')
+    return render_template('home.html', list_of_sources=FILE_SOURCE)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
